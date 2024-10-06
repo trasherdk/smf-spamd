77eeb9bd6e44836f192324c0cd27dbf69b8dce6b
trasherdk <trasherdk@gmail.com>
2024-10-06 08:41:50 +0200

    Bump Maximal message size to 1 MB

--- smf-spamd/smf-config.h
+++ smf-spamd/smf-config.h
@@ -5,36 +5,35 @@
  */
 
 /* Version */
-#define VERSION	"1.3.6"
+#define VERSION "1.3.6"
 
 /* Hosts/Networks whitelist (extended regex format) */
-#define WHITE_LIST	"(^127\\.|^192\\.168\\.|^10\\.|86\\.48\\.96\\.)"
+#define WHITE_LIST "(^127\\.|^192\\.168\\.|^10\\.|86\\.48\\.96\\.)"
 
 /* Maximal message size */
-#define MAX_SIZE	524288 /* bytes (512KB)*/
+#define MAX_SIZE 1048576 /* bytes (1MB)*/
 
 /* Probable SPAM e-Mail messages Subject tagging */
-#define TAG_SUBJECT	1 /* set 0 to disable */
+#define TAG_SUBJECT 1 /* set 0 to disable */
 
 /* Extra SPAM e-Mail messages */
-#define EXTRA_SPAM	10 /* SpamAssassin SPAM score value */
+#define EXTRA_SPAM 10 /* SpamAssassin SPAM score value */
 
 /* Contact e-Mail address for rejected extra SPAM e-Mail messages */
-#define CONTACT_ADDRESS	"notspam@yourdomain.tld" /* "all_spam_to" from your SpamAssassin local.cf */
+#define CONTACT_ADDRESS "notspam@yourdomain.tld" /* "all_spam_to" from your SpamAssassin local.cf */
 
 /* Probable SPAM e-Mail messages quarantine */
-#define REDIRECT_SPAM	0 /* set 1 to enable */
+#define REDIRECT_SPAM 0 /* set 1 to enable */
 
 /* Probable SPAM e-Mail messages collector (if quarantine is not active) */
-#define COPY_SPAM	0 /* set 1 to enable */
+#define COPY_SPAM 0 /* set 1 to enable */
 
 /* Quarantine/Collector mailbox */
-#define SPAM_BOX	"spam@yourdomain.tld" /* should be corrected carefully */
+#define SPAM_BOX "spam@yourdomain.tld" /* should be corrected carefully */
 
 /* SpamAssassin daemon listen here */
-#define SPAMD_PORT	783
-#define SPAMD_ADDRESS	"127.0.0.1"
+#define SPAMD_PORT 783
+#define SPAMD_ADDRESS "127.0.0.1"
 
 /* Syslog facility */
-#define SYSLOG_FACILITY	LOG_MAIL
-
+#define SYSLOG_FACILITY LOG_MAIL
